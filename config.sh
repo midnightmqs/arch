6ce49b4de136373c95ac0cde5e5ffe3accfe6a29
midnightmqs <midnightmarquess@proton.me>
2025-04-04 18:23:07 +0200

    ddd

---
+++ config.sh
@@ -0,0 +1,27 @@
+###############################################
+# INSTALL CONFIG                              #
+############################################### 
+hostname=magnesium
+username=midnight
+user_password=password
+root_password=password
+
+disk=/dev/nvme0n1
+use_btrfs=true
+use_encryption=true
+encryption_password=password
+cryptroot="${hostname}_root"
+
+timezone=Europe/Budapest
+locale="en_US.UTF-8 UTF-8"
+language="en_US.UTF-8"
+keymap=us
+
+bootloader_id="Arch Linux"
+
+ucode=intel
+
+install_display=true
+display_drivers="mesa vulkan-intel intel-media-driver"
+
+configure_battery=true
